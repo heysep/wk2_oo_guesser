1938f326ac932c5560b425ea08258a55270f3533
heysep <sdvakili@gmail.com>
2015-09-30 16:26:14 -0400

    Added feature to say warmer or colder depending on if guess is closer or further away to num than last guess.

--- oo_guesser_2.rb
+++ oo_guesser_2.rb
@@ -13,21 +13,32 @@ class GuessingGame
 	def play
 		count = 1
 		result = :first
-		number = rand(1..100)
+		num = rand(1..100)
 
 		guess = @player.get_guess(result)
-		
-		until guess == number
-			puts "The number is #{number} and the guess is #{guess}."
-			if guess > number
+		last_guess = guess
+
+		until guess == num
+			
+			puts "The number is #{num} and the guess is #{guess}."
+
+			if (last_guess - num).abs < (guess - num).abs
+				puts ":( Colder! But, WHY!"
+			else
+				puts "Warmer :) Oh, YEAH."
+			end
+
+			if guess > num
 				result = :high
 			else
 				result = :low
 			end
+			last_guess = guess
 			guess = @player.get_guess(result)
 			count += 1
 		end
-		puts "The number was #{number} and you guessed #{guess}."
+
+		puts "The number was #{num} and you guessed #{guess}."
 		puts "\nYou win! It took you >>:: #{count} ::<< tries."
 		puts "\nGo'on have you a beer, boy, (or lady ;)"
 			
